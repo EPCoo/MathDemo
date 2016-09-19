f9b3b63eb45bba681088a53664bd88e027c5e4dd
shun <wangshun8888@gmail.com>
2016-09-19 22:07:58 +0800

    Initial Commit

---
+++ MathDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MathDemo
+//
+//  Created by 王顺 on 16/9/19.
+//  Copyright © 2016年 ws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MathDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MathDemo
+//
+//  Created by 王顺 on 16/9/19.
+//  Copyright © 2016年 ws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
